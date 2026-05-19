7d7e62e5e4f21cd8040b482cd4e88bb69264169b
Laura Fox <laura.doevix@gmail.com>
2026-05-19 12:59:57 -0600

    start on adding dedicated json loader

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod mass;
 pub mod spring;
 pub mod world;
 pub mod model;
+pub mod loader;
 
 pub use v2d::V2D;
 pub use mass::Mass;
@@ -11,4 +12,4 @@ pub use world::World;
 pub use world::WorldConfig;
 pub use world::Boundary;
 pub use model::Model;
-
+pub use loader::Loader;
