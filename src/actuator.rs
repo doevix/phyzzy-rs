336c3cbc34a00b4b9051ec454a7ffc30f25408e9
Laura Fox <laura.doevix@gmail.com>
2026-05-23 03:48:44 -0600

    remove commented code

--- src/actuator.rs
+++ src/actuator.rs
@@ -207,12 +207,10 @@ impl BladderActuation for MassActuator {
         match self {
             // Modify spring's restlength according to waveform.
             Self::MassBalloon { mass, phase, sense, base_radius, multiplier } => {
-                // masses[*mass].r = *base_radius * (2.0 * *multiplier + (wave_amplitude * *sense * *multiplier) * (angle + *phase).sin())
                 masses[*mass].r = *base_radius * (1.0 + *multiplier * waveform(wave_amplitude, *sense, angle, *phase))
             },
             // Modify spring's springyness according to waveform.
             Self::MassTank { mass, phase, sense, base_mass , multiplier } => {
-                // masses[*mass].m = *base_mass * (2.0 * *multiplier + (wave_amplitude * *sense * *multiplier) * (angle + *phase).sin())
                 masses[*mass].m = *base_mass * (1.0 + *multiplier * waveform(wave_amplitude, *sense, angle, *phase))
             },
         }
